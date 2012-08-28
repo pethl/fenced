0a8d1f6bb84baef69a8a1dd0359c8c83b02c2130
pethl <admin@fencedweller.com>
2012-08-28 17:29:45 +0100

    revert admin to normal

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :fullname, :email, :password, :password_confirmation, :twittername, :yob, :admin
+  attr_accessible :fullname, :email, :password, :password_confirmation, :twittername, :yob
   has_secure_password
    has_many :dilemmas, dependent: :destroy
 
